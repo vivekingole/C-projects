e072e07b6e53ae79e56b2beb2d4b25609f3c69a7
Vivek Ingole <38009526+vivekingole@users.noreply.github.com>
2018-07-27 18:11:11 +0530

    Add files via upload

---
+++ Car game project/settings.bat
@@ -0,0 +1,8 @@
+copy c:\tc\lib\CS.LIB c:\tc\CS.LIB
+del c:\tc\lib\CS.LIB
+copy car.h c:\tc\include\car.h
+copy car44.cpp c:\tc\include\car44.cpp
+copy CS.LIB c:\tc\lib\CS.LIB
+del car.h
+del car44.cpp
+del CS.LIB
