641b8fead35bceec0e5f436520c54dccde0324b8
Vivek Ingole <38009526+vivekingole@users.noreply.github.com>
2018-07-27 18:12:59 +0530

    Add files via upload

---
+++ Paint application/settings.bat
@@ -0,0 +1,8 @@
+copy c:\tc\lib\CS.LIB c:\tc\CS.LIB
+del c:\tc\lib\CS.LIB
+copy paint.h c:\tc\include\paint.h
+copy paint.cpp c:\tc\include\paint.cpp
+copy CS.LIB c:\tc\lib\CS.LIB
+del paint.h
+del paint.cpp
+del CS.LIB
